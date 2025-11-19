20d868d9fbf992e7e5e6ce4d1d1b58c6e50c7c82
takaoka sayuko <sp7y3dx9snn@gmail.com>
2025-11-19 12:14:56 +0900

    feat:ヘッダーUI作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # ページごとにタイトルを変更
+  def full_title(page_title = "")
+    base_title = "COACHTECHフリマアプリ"
+    if !page_title
+      base_title
+    else
+      "#{page_title} - #{base_title}"
+    end
+  end
 end
